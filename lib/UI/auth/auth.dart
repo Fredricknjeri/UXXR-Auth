390f1d1731534e5eeac7eb1e93d370d7ee7b30a3
Fredricknjeri <fredricknjeri64@gmail.com>
2022-04-25 19:42:14 +0300

    initial commit

---
+++ lib/UI/auth/auth.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ChooseAuth extends StatefulWidget {
+  const ChooseAuth({ Key? key }) : super(key: key);
+
+  @override
+  State<ChooseAuth> createState() => _ChooseAuthState();
+}
+
+class _ChooseAuthState extends State<ChooseAuth> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
