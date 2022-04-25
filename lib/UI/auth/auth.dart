af76e8fde785ca7a05b6e34e112ddb243d26dbf0
Fredricknjeri <fredricknjeri64@gmail.com>
2022-04-25 23:55:51 +0300

    auth screen

--- lib/UI/auth/auth.dart
+++ lib/UI/auth/auth.dart
@@ -1,7 +1,13 @@
+import 'package:country_code_picker/country_code_picker.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import '../../components/colors.dart';
 
 class ChooseAuth extends StatefulWidget {
-  const ChooseAuth({ Key? key }) : super(key: key);
+  const ChooseAuth({Key? key}) : super(key: key);
 
   @override
   State<ChooseAuth> createState() => _ChooseAuthState();
@@ -10,8 +16,100 @@ class ChooseAuth extends StatefulWidget {
 class _ChooseAuthState extends State<ChooseAuth> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      appBar: AppBar(
+          backgroundColor: Colors.white,
+          elevation: 0,
+          leading: IconButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            icon: const Icon(Icons.arrow_back, color: Colors.black),
+          ),
+          centerTitle: true,
+          title: Text('Connect your wallet',
+              style: GoogleFonts.poppins(
+                  color: Colors.black,
+                  fontSize: 28.sp,
+                  fontWeight: FontWeight.w700))),
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 24.w),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(
+              height: 4.h,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text("We’ll send you a confirmation code",
+                  style: GoogleFonts.poppins(
+                      color: ligtTextColor,
+                      fontSize: 16.sp,
+                      fontWeight: FontWeight.w400)),
+            ),
+            const Spacer(),
+            Container(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Row(
+                    children: [
+                      Container(
+                        height: 24.h,
+                        width: 54.w,
+                        child: CountryCodePicker(
+                            onChanged: print,
+                            // Initial selection and favorite can be one of code ('IT') OR dial_code('+39')
+                            initialSelection: 'IT',
+                            favorite: const ['+39', 'FR'],
+                            // optional. Shows only country name and flag
+                            showCountryOnly: false,
+                            // optional. Shows only country name and flag when popup is closed.
+                            showOnlyCountryWhenClosed: false,
+                            // optional. aligns the flag and the Text left
+                            alignLeft: false,
+                            hideMainText: true),
+                      ),
+                      SizedBox(
+                        height: 34.h,
+                        width: 124.w,
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: TextField(
+                            decoration: const InputDecoration(
+                                labelText: "Enter your number",
+                                labelStyle: TextStyle()),
+                            keyboardType: TextInputType.number,
+                            inputFormatters: <TextInputFormatter>[
+                              FilteringTextInputFormatter.digitsOnly
+                            ], // Only numbers can be entered
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: TextField(
+                      decoration: const InputDecoration(
+                          labelText: "Enter your number",
+                          labelStyle: TextStyle()),
+                      keyboardType: TextInputType.number,
+                      inputFormatters: <TextInputFormatter>[
+                        FilteringTextInputFormatter.digitsOnly
+                      ], // Only numbers can be entered
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 184.h,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
---
+++ lib/components/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const textColor = Colors.black;
+const ligtTextColor = Color(0xFF373737);
+
+
+
